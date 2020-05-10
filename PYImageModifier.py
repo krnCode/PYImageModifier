86a0cc8ec758a7d456573489e8747eaaf5d4330d
kornflakes <61383309+kornflakes@users.noreply.github.com>
2020-05-10 00:57:20 -0300

    Updated script

--- PYImageModifier.py
+++ PYImageModifier.py
@@ -17,19 +17,17 @@ for rawImage in os.listdir(rawDir):
     rgbImg = img.convert('RGB')
     img_sharpen = rgbImg.filter(ImageFilter.SHARPEN)
     img_sharpen.save(modDir + '/' + rawImage[:-4] + '-sharp.jpg')
-    img_blur = rgbImg.filter(ImageFilter.BLUR
+    img_blur = rgbImg.filter(ImageFilter.BLUR)
     img_blur.save(modDir + '/' + rawImage[:-4] + '-blur.jpg')
-    img_smooth=rgbImg.filter(ImageFilter.SMOOTH)
-    # img_smooth.save(modDir + '/' + rawImage[:-4] + '-smooth.jpg')
-    img_contour=rgbImg.filter(ImageFilter.CONTOUR)
-    # img_contour.save(modDir + '/' + rawImage[:-4] + '-contour.jpg')
-    img_detail=rgbImg.filter(ImageFilter.DETAIL)
-    # img_detail.save(modDir + '/' + rawImage[:-4] + '-detail.jpg')
-    img_emboss=rgbImg.filter(ImageFilter.EMBOSS)
+    img_smooth = rgbImg.filter(ImageFilter.SMOOTH)
+    img_smooth.save(modDir + '/' + rawImage[:-4] + '-smooth.jpg')
+    img_contour = rgbImg.filter(ImageFilter.CONTOUR)
+    img_contour.save(modDir + '/' + rawImage[:-4] + '-contour.jpg')
+    img_detail = rgbImg.filter(ImageFilter.DETAIL)
+    img_detail.save(modDir + '/' + rawImage[:-4] + '-detail.jpg')
+    # img_emboss = rgbImg.filter(ImageFilter.EMBOSS)
     # img_emboss.save(modDir + '/' + rawImage[:-4] + '-emboss.jpg')
-    print(f'{img} done!')
-
-
+    print(f'{rawImage} done!')
 
 # 5 - Print message telling that everything is done
-print('All done! Check the folder to see your modified images.')
+print('All done! Check the folder to see your modified images')
